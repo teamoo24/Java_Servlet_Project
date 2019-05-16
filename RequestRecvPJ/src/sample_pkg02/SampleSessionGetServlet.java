bf91ad4ec0eddb1900f341eb15a2500792165a2c
한경수 <601107w@gmail.com>
2019-05-16 19:30:31 +0900

    Session IDの生成と取得プログラム作成

---
+++ RequestRecvPJ/src/sample_pkg02/SampleSessionGetServlet.java
@@ -0,0 +1,52 @@
+package sample_pkg02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SampleSessionGetServlet
+ */
+@WebServlet("/SampleSessionGetServlet")
+public class SampleSessionGetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SampleSessionGetServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html; charset=UTF-8");
+
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+	    HttpSession session = request.getSession();
+
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>セッションIDの取得</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+	    out.println("<h1>セッションIDの取得(ページ2)</h1>");
+	    out.println("<p>セッションIDは「" + session.getId() + "」です。</p>");
+	    out.println("<a href=\"SampleSessionCtl\">前のサーブレットに戻る</a>");
+	    out.println("</body>");
+	    out.println("</html>");
+	}
+
+}
